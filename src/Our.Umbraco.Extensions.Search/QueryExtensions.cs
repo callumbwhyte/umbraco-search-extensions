a659d4686da16511ae694a21168a41e6ea4b37b9
Callum Whyte <hey@callumwhyte.com>
2020-08-24 10:36:24 +0100

    Helpful querying extensions: HasTemplate, IsPublished, IsVisible

---
+++ src/Our.Umbraco.Extensions.Search/QueryExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Examine.Search;
+
+namespace Our.Umbraco.Extensions.Search
+{
+    public static class QueryExtensions
+    {
+        /// <summary>
+        /// Query documents with the specified template ID assigned
+        /// </summary>
+        /// <remarks>
+        /// If no <paramref name="templateId" /> is given, queries for documents without a template ID assigned
+        /// </remarks>
+        public static IBooleanOperation HasTemplate(this IQuery query, int templateId = 0)
+        {
+            if (templateId > 0)
+            {
+                return query.Field("templateID", templateId);
+            }
+
+            return query.GroupedNot(new[] { "templateID" }, "0");
+        }
+
+        /// <summary>
+        /// Query documents marked as published
+        /// </summary>
+        public static IBooleanOperation IsPublished(this IQuery query)
+        {
+            return query.Field("__Published", "y");
+        }
+
+        /// <summary>
+        /// Query documents marked as visible
+        /// </summary>
+        /// <remarks>
+        /// A document is marked as visible when <c>umbracoNaviHide</c> is set to <c>false</c>
+        /// </remarks>
+        public static IBooleanOperation IsVisble(this IQuery query)
+        {
+            return query.GroupedNot(new[] { "umbracoNaviHide" }, "1");
+        }
+    }
+}
