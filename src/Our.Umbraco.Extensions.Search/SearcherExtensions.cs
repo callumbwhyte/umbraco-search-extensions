cbaf62aa44336331485dc8945508dc51b888897d
Callum Whyte <hey@callumwhyte.com>
2020-08-24 10:38:36 +0100

    Extension for ISearcher to easily create queries for only published content

---
+++ src/Our.Umbraco.Extensions.Search/SearcherExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Examine;
+using Examine.Search;
+
+namespace Our.Umbraco.Extensions.Search
+{
+    public static class SearcherExtensions
+    {
+        /// <summary>
+        /// Creates a query with predefined criteria for published content
+        /// </summary>
+        public static IBooleanOperation CreatePublishedQuery(this ISearcher searcher, string category = null, BooleanOperation defaultOperation = BooleanOperation.And)
+        {
+            var query = searcher
+                .CreateQuery(category, defaultOperation)
+                .IsPublished();
+
+            query.And().IsVisble();
+
+            query.And().HasTemplate();
+
+            return query;
+        }
+    }
+}
