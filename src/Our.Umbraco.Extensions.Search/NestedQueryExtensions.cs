90a698cc11e0f1d96a18ae4f2978c1300d83193d
Callum Whyte <hey@callumwhyte.com>
2020-11-09 04:09:29 +0000

    Adding query extensions for nested queries

---
+++ src/Our.Umbraco.Extensions.Search/NestedQueryExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Examine.Search;
+
+namespace Our.Umbraco.Extensions.Search
+{
+    public static class NestedQueryExtensions
+    {
+        /// <summary>
+        /// Query documents with the specified template ID assigned
+        /// </summary>
+        /// <remarks>
+        /// If no <paramref name="templateId"/> is given, queries for documents without a template ID assigned
+        /// </remarks>
+        public static INestedBooleanOperation HasTemplate(this INestedQuery query, int templateId = 0)
+        {
+            return query.Field("templateID", templateId.ToString());
+        }
+
+        /// <summary>
+        /// Query documents marked as published
+        /// </summary>
+        public static INestedBooleanOperation IsPublished(this INestedQuery query)
+        {
+            return query.Field("__Published", "y");
+        }
+
+        /// <summary>
+        /// Query documents marked as visible
+        /// </summary>
+        /// <remarks>
+        /// A document is marked as visible when <c>umbracoNaviHide</c> is set to <c>false</c>
+        /// </remarks>
+        public static INestedBooleanOperation IsVisble(this INestedQuery query)
+        {
+            return query.Field("umbracoNaviHide", "0");
+        }
+    }
+}
