29b8fc575bea0f2fa4efe3255a6b44519fbdf201
Callum Whyte <hey@callumwhyte.com>
2020-11-09 18:29:58 +0000

    Ensuring components run after Examine is configured

--- src/Our.Umbraco.Extensions.Search/Startup/IndexComposer.cs
+++ src/Our.Umbraco.Extensions.Search/Startup/IndexComposer.cs
@@ -1,13 +1,15 @@
 ﻿using Umbraco.Core;
 using Umbraco.Core.Composing;
+using Umbraco.Web.Search;
 
 namespace Our.Umbraco.Extensions.Search.Startup
 {
+    [ComposeAfter(typeof(ExamineComposer))]
     public class IndexComposer : IUserComposer
     {
         public void Compose(Composition composition)
         {
-            composition.Components().Append<IndexComponent>();
+            composition.Components().InsertAfter<ExamineComponent, IndexComponent>();
         }
     }
 }
