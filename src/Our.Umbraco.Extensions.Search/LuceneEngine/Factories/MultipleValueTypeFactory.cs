41e315dce7b421aed4d61abd6cfa4b13fc5c2b46
Callum Whyte <hey@callumwhyte.com>
2021-02-21 22:17:25 +0000

    MultipleValueTypeFactory for indexing field values as multiple types

---
+++ src/Our.Umbraco.Extensions.Search/LuceneEngine/Factories/MultipleValueTypeFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Examine.LuceneEngine;
+using Examine.LuceneEngine.Indexing;
+using Our.Umbraco.Extensions.Search.LuceneEngine.ValueTypes;
+
+namespace Our.Umbraco.Extensions.Search.LuceneEngine.Factories
+{
+    public class MultipleValueTypeFactory : IFieldValueTypeFactory
+    {
+        private readonly Func<string, IEnumerable<IIndexFieldValueType>> _factories;
+
+        public MultipleValueTypeFactory(Func<string, IEnumerable<IIndexFieldValueType>> factories)
+        {
+            _factories = factories;
+        }
+
+        public IIndexFieldValueType Create(string fieldName)
+        {
+            return new MultiValueType(fieldName, _factories(fieldName));
+        }
+    }
+}
---
+++ src/Our.Umbraco.Extensions.Search/LuceneEngine/ValueTypes/MultiValueType.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Examine.LuceneEngine.Indexing;
+using Lucene.Net.Documents;
+
+namespace Our.Umbraco.Extensions.Search.LuceneEngine.ValueTypes
+{
+    internal class MultiValueType : IndexFieldValueTypeBase
+    {
+        private readonly IEnumerable<IIndexFieldValueType> _fieldTypes;
+
+        public MultiValueType(string fieldName, IEnumerable<IIndexFieldValueType> fieldTypes)
+            : base(fieldName)
+        {
+            _fieldTypes = fieldTypes;
+        }
+
+        protected override void AddSingleValue(Document doc, object value)
+        {
+            foreach (var fieldType in _fieldTypes)
+            {
+                var fields = doc.GetFields(fieldType.FieldName);
+
+                if (fields.Any() == true)
+                {
+                    doc.RemoveFields(fieldType.FieldName);
+
+                    foreach (var field in fields)
+                    {
+                        fieldType.AddValue(doc, field.StringValue);
+                    }
+                }
+                else
+                {
+                    fieldType.AddValue(doc, value);
+                }
+            }
+        }
+    }
+}
