6f6d4b89f6f1c76feb255c1dc360c2b9953e31d4
Callum Whyte <hey@callumwhyte.com>
2025-11-22 08:49:24 +0000

    Ensure SearchComposer runs after Umbraco's Examine configuration, remove support for Umbraco 15
    This ensures Umbraco won't overwrite the configuration by Search Extensions

--- src/Our.Umbraco.Extensions.Search/Composing/SearchComposer.cs
+++ src/Our.Umbraco.Extensions.Search/Composing/SearchComposer.cs
@@ -3,12 +3,18 @@ using Our.Umbraco.Extensions.Search.Helpers;
 using Our.Umbraco.Extensions.Search.Mappers;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
+#if NET9_0_OR_GREATER
+using Umbraco.Cms.Infrastructure.Examine;
+#endif
 #if !NET8_0_OR_GREATER
 using Umbraco.Cms.Web.Common.DependencyInjection;
 #endif
 
 namespace Our.Umbraco.Extensions.Search.Composing
 {
+#if NET9_0_OR_GREATER
+    [ComposeAfter(typeof(AddExamineComposer))]
+#endif
     public class SearchComposer : IComposer
     {
         public void Compose(IUmbracoBuilder builder)
