f174c521c9904778a688986e73d7e1bd3bdf30c2
Callum Whyte <hey@callumwhyte.com>
2026-01-31 05:22:26 +0000

    Support multiple separators in lists

--- src/Our.Umbraco.Extensions.Search/ValueTypes/ListValueType.cs
+++ src/Our.Umbraco.Extensions.Search/ValueTypes/ListValueType.cs
@@ -1,30 +1,30 @@
-﻿using Examine;
-using Examine.Lucene.Indexing;
-using Lucene.Net.Analysis;
+﻿using System;
 using Lucene.Net.Documents;
 using Microsoft.Extensions.Logging;
-using Our.Umbraco.Extensions.Search.Analysis;
 
 namespace Our.Umbraco.Extensions.Search.ValueTypes
 {
     public class ListValueType : IndexFieldValueTypeBase
     {
-        public ListValueType(string fieldName, ILoggerFactory loggerFactory, char separator = ',', bool store = true)
+        public ListValueType(string fieldName, ILoggerFactory loggerFactory, bool store = true)
             : base(fieldName, loggerFactory, store)
         {
-            Separator = separator;
+
         }
 
-        public char Separator { get; }
+        public string[] Separators { get; } = new[] { ",", "\n", "\r\n" };
 
         protected override void AddSingleValue(Document doc, object value)
         {
             if (value is string stringValue)
             {
-                doc.Add(new TextField(FieldName, stringValue, Store ? Field.Store.YES : Field.Store.NO));
+                var items = stringValue.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var item in items)
+                {
+                    doc.Add(new TextField(FieldName, item, Store ? Field.Store.YES : Field.Store.NO));
+                }
             }
         }
-
-        public override Analyzer Analyzer => new WhitespaceSeparatorAnalyzer(LuceneInfo.CurrentVersion, Separator);
     }
 }
